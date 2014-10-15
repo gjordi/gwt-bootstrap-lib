1ec0f25b794934dd2a3f0634c1d7c49be61993e7
Gjordi McIntyre <gjordi@bytebybyte.com>
2014-10-15 11:15:27 -0600

    Added entry point configuration.

--- README.md
+++ README.md
@@ -1,4 +1,27 @@
 gwt-bootstrap-lib
 =================
 
-Bootstrap Library for GWT Projects
+[Bootstrap](http://getbootstrap.com) library for GWT projects.
+
+# Usage
+
+Add the dependency to your Maven POM:
+
+```xml
+<dependency>
+	<groupId>com.bytebybyte.gwt</groupId>
+	<artifactId>bootstrap-lib</artifactId>
+	<version>3.2.0</version>
+	<scope>provided</scope>
+</dependency>
+```
+
+And to your .gwt.xml file:
+
+```xml
+<module>
+	...
+	<inherits name="com.bytebybyte.gwt.bootstrap.BootstrapLib" />
+	...
+</module>
+```
--- src/main/java/com/bytebybyte/gwt/bootstrap/client/BootstrapLibConfigurerEntryPoint.java
+++ src/main/java/com/bytebybyte/gwt/bootstrap/client/BootstrapLibConfigurerEntryPoint.java
@@ -1,5 +1,24 @@
 package com.bytebybyte.gwt.bootstrap.client;
 
-public class BootstrapLibConfigurerEntryPoint {
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.bytebybyte.gwt.bootstrap.client.resources.BootstrapLibClientBundle;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.ScriptInjector;
+
+public class BootstrapLibConfigurerEntryPoint implements EntryPoint {
+
+	protected Logger logger = Logger.getLogger(BootstrapLibConfigurerEntryPoint.class.getName());
+
+	@Override
+	public void onModuleLoad() {
+
+		BootstrapLibClientBundle bundle = BootstrapLibClientBundle.INSTANCE;
+
+		logger.log(Level.INFO, "Injecting bootstrap javascript script element.");
+
+		ScriptInjector.fromString(bundle.bootstrap().getText()).setWindow(ScriptInjector.TOP_WINDOW).inject();
+	}
 
 }
